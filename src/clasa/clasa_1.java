0e6422fff3204c264982fd792e665856980f5764
Sorin Dunareanu <sorinduni@gmail.com>
2014-10-26 14:26:01 +0200

    first commit

---
+++ src/clasa/clasa_1.java
@@ -0,0 +1,11 @@
+package clasa;
+
+/**
+ * Created by Sorin on 10/26/2014.
+ */
+public class clasa_1 {
+    public void print_msg ()
+    {
+        System.out.println("Primul mesaj!");
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Sorin on 10/26/2014.
+ */
+import clasa.clasa_1;
+
+public class main {
+    public static void main (String[] args){
+        clasa_1 cls = new clasa_1();
+        cls.print_msg();
+    }
+}
